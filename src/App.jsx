3e33fe126f975d3d8fd3bfd52cf02189791d5466
Denis <denis.tolmachev@gmail.com>
2022-07-21 21:54:43 +0300

    correct contacts state

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,7 @@ const KEY = 'contacts';
 
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(
-      window.localStorage.getItem(KEY) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
+    return JSON.parse(localStorage.getItem(KEY) ?? []);
   });
   const [filter, setFilter] = useState('');
 
